570a2408d00a4b88896128c292b26d1f7f1b08b6
Marcus Nørregaard Hartmann <mah016@edu.zealand.dk>
2023-11-07 13:21:46 +0100

    EditDrikkevarer

---
+++ Pages/Drikkevarerer/EditDrikkevarer.cshtml.cs
@@ -0,0 +1,77 @@
+using menukort.model;
+using menukort.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace menukort.Pages.Drikkevarerer
+{
+    public class EditDrikkevarerModel : PageModel
+    {
+        public class EditDrikkevarerModel : PageModel
+        {
+            private IDrikkevarerRepository _repo;
+
+            public EditDrikkevarerModel(IDrikkevarerRepository repo)
+            {
+                _repo = repo;
+            }
+
+            [BindProperty]
+            [Required(ErrorMessage = "Der skal være et nummer")]
+            public int? NytDrikkevarerNummer { get; set; }
+
+
+            [BindProperty]
+            [Required(ErrorMessage = "Der skal være et navn")]
+            [StringLength(1000, MinimumLength = 2, ErrorMessage = "Der skal være mindst to tegn i et navn")]
+            public string NytDrikkevarerNavn { get; set; }
+
+
+            [BindProperty]
+            public string NytDrikkevarerStørrelse { get; set; }
+
+
+            [BindProperty]
+            [Required(ErrorMessage = "Der skal være en pris")]
+            public double NytDrikkevarerPris { get; set; }
+
+
+            [BindProperty]
+            public bool NytDrikkevarerAlkohol { get; set; }
+
+
+            public void OnGet(int nummer)
+            {
+                Drikkevarer drikkevarer = _repo.HentDrikkevarer(nummer);
+
+                NytDrikkevarerNummer = drikkevarer.Nummer;
+                NytDrikkevarerNavn = drikkevarer.Navn;
+                NytDrikkevarerStørrelse = drikkevarer.Størrelse;
+                NytDrikkevarerPris = drikkevarer.Pris;
+                NytDrikkevarerAlkohol = drikkevarer.Alkohol;
+            }
+            public IActionResult OnPostChange()
+            {
+                if (!ModelState.IsValid)
+                {
+                    return Page();
+                }
+
+                Drikkevarer drikkevarer = _repo.HentDrikkevarer(NytDrikkevarerNummer ?? 0);
+
+                drikkevarer.Navn = NytDrikkevarerNavn;
+                drikkevarer.Størrelse = NytDrikkevarerStørrelse;
+                drikkevarer.Pris = NytDrikkevarerPris;
+                drikkevarer.Alkohol = NytDrikkevarerAlkohol;
+
+                return RedirectToPage("Index");
+            }
+
+            public IActionResult OnPostCancel()
+            {
+                return RedirectToPage("Index");
+            }
+        }
+    }
+}
