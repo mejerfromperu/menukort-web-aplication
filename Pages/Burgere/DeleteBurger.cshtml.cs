5d6cc3fc606cbe28496a94296f0c8db516988840
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2023-11-07 09:11:33 +0100

    Delete burger fixed

--- Pages/Burgere/DeleteBurger.cshtml.cs
+++ Pages/Burgere/DeleteBurger.cshtml.cs
@@ -1,3 +1,5 @@
+using menukort.model;
+using menukort.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,38 @@ namespace menukort.Pages.Burgere
 {
     public class DeleteBurgerModel : PageModel
     {
-        public void OnGet()
+
+        private IBurgerRepository _repo;
+
+
+        public DeleteBurgerModel(IBurgerRepository repo)
         {
+            _repo = repo;
         }
+
+        public Burger Burger { get; private set; }
+
+
+        public IActionResult OnGet(int nummer)
+        {
+            Burger = _repo.HentBurger(nummer);
+
+
+            return Page();
+        }
+
+        public IActionResult OnPostDelete(int nummer)
+        {
+            _repo.Slet(nummer);
+
+            return RedirectToPage("Index");
+        }
+
+        public IActionResult OnPostFortryd(int nummer)
+        {
+            return RedirectToPage("Index");
+        }
+
+
     }
 }
